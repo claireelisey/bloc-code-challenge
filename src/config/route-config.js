b24232605276a6519a465e0555ba98352f905a4c
Claire Ouellette <claireelisey@gmail.com>
2018-11-28 21:51:24 -0600

    adding user resource

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,9 +4,11 @@ module.exports = {
 
         const staticRoutes = require("../routes/static");
         const postRoutes = require("../routes/posts");
+        const userRoutes = require("../routes/users");
 
         app.use(staticRoutes);
         app.use(postRoutes);
+        app.use(userRoutes);
         
     }
 
